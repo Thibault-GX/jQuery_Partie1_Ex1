25785512962923abb800139bf61402459fa1f65c
Thibault-GX <thibault.grimoux@gmail.com>
2019-11-04 11:50:43 +0100

    Affinage des commentaires du script.js, pour plus d'user friendliness.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-//On sélectionne les balises p / <p> / paragraphes et on leur demande d'aller se cacher.
+// [1] On sélectionne les balises p / <p> / paragraphes et on leur demande d'aller se cacher.
 //$("p").hide();
 
-//Une autre méthode consisterait à sélectionner la classe de ce <p>, afin d'éviter de potentiels conflits avec d'autres paragraphes.
+// [2] Une autre méthode consisterait à sélectionner la classe de ce <p>, afin d'éviter de potentiels conflits avec d'autres paragraphes.
 //$("#text").hide();
 
-//Et si on veut pousser un peu le vice, on peut lui demander de se cacher seulement lors d'un clic de souris. Fonctionnera aussi avec le sélecteur #text
+// [3] Et si on veut pousser un peu le vice, on peut lui demander de se cacher seulement lors d'un clic de souris. Fonctionnera aussi avec le sélecteur #text
 // $("p").on("click",function(){
 //   $(this).hide();
 // });
 
-//Et enfin, histoire de profiter d'un rendu un peu moins "dégueulasse", on peut même se faire un petit bouton pour rendre le tout plus esthétique.
+// [4] Et enfin, histoire de profiter d'un rendu un peu moins "dégueulasse", on peut même se faire un petit bouton pour rendre le tout plus esthétique.
 $("#button").on("click",function(){
   $("#text").hide();
 });
